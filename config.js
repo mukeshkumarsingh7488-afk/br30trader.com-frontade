f87e3ee6591a58dc7f8bd958012f34209a5633be
Mukesh Kumar <mukeshkumarsingh7488@gmail.com>
2026-04-08 22:18:27 +0530

    🔥 fixed thumbnail field name (image → thumbnail) + cloudinary upload fi

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
+//#region MyCode
 // config.js
 const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
 
-window.API_BASE_URL = isLocal 
-    ? "http://localhost:5000" 
+window.API_BASE_URL = isLocal
+    ? "http://localhost:5000"
     : "https://my-backend-1-avpd.onrender.com"; // Yahan live URL aayega
 
-
+//#endregion
