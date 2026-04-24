c652c06030b620eb0ca83f5549dd88521fe165a9
Mukesh Kumar <mukeshkumarsingh7488@gmail.com>
2026-04-25 01:07:33 +0530

    Finalized file structure: CSS/JS separated and path issues fixed

--- js/config.js
+++ js/config.js
@@ -1,9 +1,11 @@
 //#region MyCode
 // config.js
-const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+const isLocal =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1";
 
 window.API_BASE_URL = isLocal
-    ? "http://localhost:5000"
-    : "https://my-backend-1-avpd.onrender.com"; // Yahan live URL aayega
+  ? "http://localhost:5000"
+  : "https://my-backend-1-avpd.onrender.com";
 
 //#endregion
