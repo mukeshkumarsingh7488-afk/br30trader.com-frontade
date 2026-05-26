62b5750e9e354d550b5ddbed309d6d1f1ae5b6b9
Mukesh Kumar <mukeshkumarsingh7488@gmail.com>
2026-05-26 17:16:28 +0530

    Fix global dark background and remove white page flash

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <div style={{ background: "#050505", minHeight: "100vh", width: "100%" }}>
+      <Router>
+        <App />
+      </Router>
+    </div>
   </React.StrictMode>,
 );
